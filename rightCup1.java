0d63988caba589f5858013180b7ca735632c1ca6
JuanChavez121 <143749918+JuanChavez121@users.noreply.github.com>
2023-11-13 14:25:45 -0500

    Add files via upload

---
+++ rightCup1.java
@@ -0,0 +1,58 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class rightCup1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class rightCup1 extends Actor
+{
+    /**
+     * Act - do whatever the rightCup1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public boolean isMovingUp=false;
+    private int moveSpeedUp=2;
+    private int delayCounter = 0;
+    private int moveSpeed=1;
+    private easyTable world;
+    public rightCup1()
+    {
+        GreenfootImage letterMImage = getImage();
+        
+        int newWidth=100; 
+        int newHeight=150; 
+        letterMImage.scale(newWidth, newHeight);
+        setImage(letterMImage);
+
+    
+        
+    }
+    public rightCup1(easyTable world) {
+        this.world = world; 
+    }
+    public void act(){
+    if (getWorld() instanceof easyTable) {
+        easyTable world = (easyTable) getWorld();
+        int numSwaps = world.numSwaps;
+        if (Greenfoot.mouseClicked(this) && numSwaps == 0) {
+            while (getY() > 475 && isMovingUp == false) {
+                setLocation(getX(), getY() - moveSpeedUp);
+                Greenfoot.delay(1);
+            }
+            world.showText("You lose!", 500, 300);
+            if (getY() == 475) {
+                isMovingUp = true;
+            }
+        } else if (getY() < 525 && isMovingUp == true) {
+            delayCounter++;
+            if (delayCounter >= 120) { 
+                setLocation(getX(), getY() + moveSpeed);
+                Greenfoot.delay(1);
+            }
+            
+        }
+    }
+    }
+}
